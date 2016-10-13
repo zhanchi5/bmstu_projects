4dcc2bc066c6d46b6d1d667b5e28ed36c54bc683
zhanchi5 <zhanchi5@outlook.com>
2016-10-13 18:42:24 +0400

    Update solution_for_equation.cpp
    1. Инициализация переменной при объявлении.
    2. Использование getch() вместо system("Pause")

--- solution_for_equition(task_1).cpp
+++ solution_for_equition(task_1).cpp
@@ -2,16 +2,18 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <conio.h>
 // программа для вычисления значения y при вводимых x
 int main()
 {
     long double y;
-    int x;
+    int x=0;
     std::cout<<"Введите значение x: ";
     std::cin>>x;
     std::printf("y=x^2+11+sqrt((x^2+11)\n");
     y=((pow(x,2))+11)+sqrt((double(pow(x,2)+11)));
     std::printf("Ответ: y= %ld",y);
+    getch();
     return 0;
 
 }
